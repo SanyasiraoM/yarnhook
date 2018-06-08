387aab7d74a46622aab5744ead3ed7af67306c12
SanyasiraoM <sanyasirao.mopada@gmail.com>
2018-06-08 14:28:54 +0530

    Update index.js
    Only npm support

--- index.js
+++ index.js
@@ -2,78 +2,64 @@
 
 // @flow
 
-const findParentDir = require("find-parent-dir");
-const execa = require("execa");
-const { join } = require("path");
-const fs = require("fs");
+const findParentDir = require('find-parent-dir')
+const execa = require('execa')
+const { join } = require('path')
+const fs = require('fs')
 
-// environment variables
-const { YARNHOOK_BYPASS = false, YARNHOOK_DEBUG = false, YARNHOOK_DRYRUN = false } = process.env;
-
-// supported package managers and lockfile names
-const lockfileSpecs = [
-  ["yarn", "yarn.lock"],
-  ["npm", "npm-shrinkwrap.json"],
-  ["npm", "package-lock.json"],
-  ["pnpm", "shrinkwrap.yaml"]
-];
-
-function getLockfileSpec(currentDir) {
-  for (let [cmd, lockfile] of lockfileSpecs) {
-    const lockfilePath = join(currentDir, lockfile);
-    if (fs.existsSync(lockfilePath)) {
-      return { cmd, lockfilePath };
-    }
-  }
-
-  return null;
-}
+const { YARNHOOK_BYPASS = false, YARNHOOK_DEBUG = false, YARNHOOK_DRYRUN = false } = process.env
 
 if (!YARNHOOK_BYPASS) {
-  // find directories
-  const currentDir = process.cwd();
-  const gitDir = findParentDir.sync(currentDir, ".git");
+  // switch to gitdir
+  const currentDir = process.cwd()
+  const gitDir = findParentDir.sync(currentDir, '.git')
+  const npmLockPath = join(currentDir, 'package-lock.json')
 
-  // check for lockfiles
-  const lockfileSpec = getLockfileSpec(currentDir);
-
-  if (YARNHOOK_DEBUG) {
-    console.log("currentDir:", currentDir);
-    console.log("gitDir:", gitDir);
-    console.log("lockfile:", lockfileSpec);
+  // check for yarn's and npm's lockfiles
+  const CMD = 'npm'
+  const getLockfile = function () {
+    // get npm's lockfile
+    if (fs.existsSync(npmLockPath)) {
+      return npmLockPath
+    }
+    return null
   }
 
-  if (lockfileSpec !== null) {
-    // get the command and lockfile path
-    const { cmd, lockfilePath } = lockfileSpec;
+  const lockfile = getLockfile()
 
+  if (YARNHOOK_DEBUG) {
+    console.log('currentDir:', currentDir)
+    console.log('gitDir:', gitDir)
+    console.log('lockfile:', lockfile)
+    console.log('CMD:', CMD)
+  }
+
+  if (lockfile !== null) {
     // run a git diff on the lockfile
-    const { stdout: output } = execa.sync(
-      "git",
-      ["diff", "HEAD@{1}..HEAD@{0}", "--", lockfilePath],
-      { cwd: gitDir }
-    );
+    const { stdout: output } = execa.sync('git', ['diff', 'HEAD@{1}..HEAD@{0}', '--', lockfile], {
+      cwd: gitDir
+    })
 
     if (YARNHOOK_DEBUG) {
-      console.log(output);
+      console.log(output, output.length > 0)
     }
 
     // if diff exists, update dependencies
     if (output.length > 0) {
       if (YARNHOOK_DRYRUN) {
         console.log(
-          `Changes to lockfile found, you should run \`${cmd} install\` if you want to have dependencies of this branch.`
-        );
+          `Changes to lockfile found, you should run \`${CMD} install\` if you want to have dependencies of this branch.`
+        )
       } else {
-        console.log(`Changes to lockfile found, running \`${cmd} install\``);
-        execa.sync(cmd, ["install"], { stdio: "inherit" });
+        console.log(`Changes to lockfile found, running \`${CMD} install\``)
+        execa.sync(CMD, ['install'], { stdio: 'inherit' })
       }
     }
   } else {
     console.log(
       "I can't seem to find a lockfile. Currently supported lockfiles are: yarn.lock, package-lock.json and shrinkwrap.yaml. Please " +
-        "open an issue at https://github.com/frontsideair/yarnhook/issues if " +
+        'open an issue at https://github.com/frontsideair/yarnhook/issues if ' +
         "you think it's my fault."
-    );
+    )
   }
 }
